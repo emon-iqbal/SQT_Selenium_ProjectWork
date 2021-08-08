8535dfd15a86abcb87d75bffdd12454fb70cba79
emon-iqbal <rasjbii@gmail.com>
2021-08-08 19:28:57 +0600

    entryPoint + SELENiUM
    Opening page of project from here.....
    Selenium automation test is also in the entrypoint or "td-app.js" file of this dummy project............

--- td-app.js
+++ td-app.js
@@ -1,15 +1,34 @@
-const express 			= require ('express');
-const upload					= require ('express-fileupload');
-const exSession			= require ('express-session');
-const exValidator		= require ('express-validator');
-const bodyParser		= require ('body-parser');
-const apiResponse 	= require("express-api-response");
+//selenium - auto navigation to our login page
+const {Builder, WebDriver}	=	require("selenium-webdriver");							//calling selenium-webdriver from "node-modules" folder
+
+async function testSelenium()												//synchronous function to give a random name as like this:  testSelenium
+{
+	let driver	=	await new Builder().forBrowser("firefox").build();		//create web driver and build brower name for automated navigation
+
+	await driver.manage().window().maximize();								//maximize the firefox browser immediately after generating the command
+	await driver.get("http://localhost:3000/");								//	after getting the command : >>"npm run travelbliss" the opening page 
+	///////////////////////////////////////////											of project will be visible immediately and automatically	
+	
+	await driver.sleep(20000);											//webpage is visible for 20 seconds with or without hands-off mouse/touchpad
+	await driver.quit();												//after exactly 20 seconds the entire browser will be closed by itself
+}																			//>>	and thats what the main wonder of SELENiUM indeed	<<//
+testSelenium();
+
+
+
+//======================== FROM HERE ... previous codeLines that have been already done before in an advanced course ========================//
+//========================	in order to develop such a different web application  ========================//
+
+const express		= require ('express');
+const upload		= require ('express-fileupload');
+const exSession	= require ('express-session');
+const bodyParser	= require ('body-parser');
 //------------------------------------------------------
-const tdLogin 								= require ('./controller/tdLogin');
-const tdRegistration 			= require ('./controller/tdReg');
-const td_dashboard 			= require  ('./controller/td_DASHBOARD');
-const td_logout 						= require  ('./controller/td_LogouT');
-const app 											= express ();																//app variables
+const tdLogin			= require('./controller/tdLogin');
+const tdRegistration	= require('./controller/tdReg');
+const td_dashboard		= require('./controller/td_DASHBOARD');
+const td_logout		= require('./controller/td_LogouT');
+const app				= express();									//app variables
 
 
 //config
@@ -43,9 +62,30 @@ app.listen (3000, () => {
 
 
 
+/* 
+	await driver.get("http://localhost:3000/").then(async function()
+	{
+		await driver.findElement(WebDriver.By.linkText("tdLOGiN")).click.then(async function()
+		{
+			await driver.navigate.tdLogin.then(async function()
+			{
+				//console.log(title);
+				await driver.sleep(20000);									
+				await driver.quit();
+			});
+		});
+
+	});
+*/
 
 
 
+/* async clickSignOut() {
+	const signOutSelector = by.css( 'button.sidebar__me-signout-button' );
+	await this.driver.sleep( 1000 );
+	await driverHelper.clickWhenClickable( this.driver, signOutSelector );
+	return await this.driver.sleep( 1000 );
+} */
 
 
 //app.use('/Halloween-IN-Rockport-MA', express.static('assets'));
